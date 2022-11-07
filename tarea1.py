4e3f701d054b6acbdc8d4c7e67c2dbc5f9704dd4
Brayan <torresacunabrayan460@gmail.com>
2022-11-06 19:57:13 -0500

    Agrega funciones de opción 2,3 y 4

--- tarea1.py
+++ tarea1.py
@@ -69,3 +69,67 @@ def validarInt(mensaje: str) -> int:
             variable = int(variable)
             return variable
         print("El valor a ingresar debe ser un número")
+
+def validarNroAutores() -> int:
+    while True:
+        nroAutores = validarInt(" Nro de autores:")
+        if nroAutores > 0:
+            return nroAutores
+        print("El libro debe tener al menos 1 autor")
+
+def crearIndice(lenLibros: int) -> int:
+    return lenLibros
+
+def leerArrayString(mensaje: str, longitud: int) -> list[str]:
+    lista = []
+    for i in range(1, longitud + 1):
+        variable = leerArrayString(mensaje + str(i) + ":")
+        lista.append(variable)
+    return lista
+
+#función de la opción 2
+
+def listar_libros(lista_libros: list) -> None:
+    datos_libro = []
+    
+    for libro in lista_libros:
+        autores = "\n".join(libro.get_autores())
+        datos_libro.append(
+            [   
+                libro.get_id(),
+                libro.get_titulo(),
+                libro.get_genero(),
+                libro.get_ISBN(),
+                libro.get_editorial(),
+                autores,
+            ]
+        )
+
+    print(datos_libro)
+
+#función de la opción 3
+
+def crear_libro(lista_libros: list[Libro]) -> Libro:
+    id = crearIndice(len(lista_libros))
+    print("Ingrese los datos del libro:")
+    titulo = validar_respuesta("Título:")
+    genero = validar_respuesta("Género: ")
+    isbn = validar_respuesta("ISBN: ")
+    editorial = validar_respuesta("Editorial: ")
+    nroAutores = validarNroAutores()
+    autores = leerArrayString("Autor :", nroAutores)
+    libroNuevo = Libro(id, titulo, genero, isbn, editorial, autores)
+    return libroNuevo
+
+#función de la opción 4
+
+def eliminar_libro(id: int, lista_libros: list[Libro]) -> None:
+    borrado = 0
+    for libro in lista_libros:
+
+        if libro.get_id() == id:
+            lista_libros.pop(lista_libros.index(libro))
+            print("El libro ha sido eliminado con exito")
+            borrado += 1
+    if borrado == 0:
+        print("El id no está en la lista")
